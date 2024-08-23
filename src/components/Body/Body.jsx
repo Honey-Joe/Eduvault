2012a1b6ff9bc6a5b0caecfc01edd15bb5c5a42e
Honey-Joe <honeyjoe943@gmail.com>
2024-08-23 21:11:53 +0530

    update aligment

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -7,7 +7,7 @@ const Body = () => {
   return (
     <>
       <div className="3xl:container mx-auto bg-[#FCFCFC]">
-        <div className="w-[85%] mx-auto grid grid-cols-1 lg:grid-cols-2">
+        <div className="w-[95%] mx-auto grid grid-cols-1 lg:grid-cols-2">
           <div>
             <YoutubeVid></YoutubeVid>
             <Navtab></Navtab>
