3d16cc4777897ec27df75ebb389807c2fa9de53e
Honey-Joe <honeyjoe943@gmail.com>
2024-08-23 21:15:55 +0530

    update buton announcemnt

--- src/components/Announcement/Announcement.jsx
+++ src/components/Announcement/Announcement.jsx
@@ -5,12 +5,12 @@ const Announcement = () => {
     <>
         <div className='2xl:container mx-auto'>
             <div className='grid grid-cols-1 py-3 px-5 bg-white rounded-lg' id='certi'>
-                <div className='flex justify-between py-2 items-center'>
+                <div className='flex justify-between py-2 items-center flex-wrap'>
                     <div>
                         <p className='font-[LexendDeca] font-bold text-[20px]'>UI/UX Course Figma</p>
                     </div>
                     <div>
-                        <p className='w-full px-3 py-3 bg-blue-700 font-[LexendDeca] text-white rounded-md items-center'>16 Jun</p>
+                        <p className='w-full py-2 px-2 sm:px-3 sm:py-3  bg-blue-700 font-[LexendDeca] text-white rounded-md items-center'>16 Jun</p>
                     </div>
                 </div>
                 <div className='flex justify-between py-3'>
