3d1bb39df5e714cf55267485a0a36c5469b090b3
Honey-Joe <honeyjoe943@gmail.com>
2024-08-24 23:50:58 +0530

    dsfdsf

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,24 @@
-import ReactDOM from "react-dom/client"
-import "./index.css" 
-import Header from "./components/Header/Header"
-import Body from "./components/Body/Body"
-import Footer from "./components/Footer/Footer"
-import Righttab from "./components/Righttab/Righttab"
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Header from "./components/Header/Header";
+import Body from "./components/Body/Body";
+import Footer from "./components/Footer/Footer";
+import Righttab from "./components/Righttab/Righttab";
 
-
-
-const Applayout = ()=>{
-  return(
+const Applayout = () => {
+  return (
     <>
-    <div>
-    <Header></Header>
-    <div>
-    <Body></Body>
-    
-    </div>
-      
-      <Footer></Footer>
-    </div>
-      
-    </>
-  )
-}
+      <div>
+        <Header></Header>
+        <div>
+          <Body></Body>
+        </div>
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<Applayout></Applayout>)
+        <Footer></Footer>
+      </div>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Applayout></Applayout>);
